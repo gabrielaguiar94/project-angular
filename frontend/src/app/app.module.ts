316d01e1b7ffe55153c29ee08411d8823e6bae60
gabrielaguiar94 <gabrielaguiar1994@hotmail.com>
2021-07-23 12:03:41 -0300

    Add home page

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -10,15 +10,19 @@ import { MatToolbarModule} from '@angular/material/toolbar';
 import { FooterComponent } from './components/template/footer/footer.component';
 
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatCardModule } from '@angular/material/card';
 import { MatListModule} from '@angular/material/list';
 import { NavComponent } from './components/template/nav/nav.component';
+import { HomeComponent } from './views/home/home.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     FooterComponent,
-    NavComponent
+    NavComponent,
+    HomeComponent
     
   ],
   imports: [
@@ -27,7 +31,8 @@ import { NavComponent } from './components/template/nav/nav.component';
     BrowserAnimationsModule,
     MatToolbarModule,
     MatSidenavModule,
-    MatListModule
+    MatListModule,
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
